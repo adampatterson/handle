9605d25750c717dac0406f2979eb5ef2f2eef4ae
Adam Patterson <hello@adampatterson.ca>
2026-03-17 10:46:16 -0600

    Cleanup

--- lib/Helpers.php
+++ lib/Helpers.php
@@ -1,6 +1,6 @@
 <?php
 // Uncommenting this would require all assets to be loaded from the Child theme.
-// It will also update the path to all of the parent asset helper functions.
+// It will also update the path to all the parent asset helper functions.
 /*
 function __t($useParent = false)
 {
